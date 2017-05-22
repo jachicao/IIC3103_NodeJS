ad7e15c76f05c45b2711bfd1d3cd219eac798a0b
jachicao <jachicao@uc.cl>
2017-05-22 15:34:44 -0400

    Update develop-deploy.sh

--- develop-deploy.sh
+++ develop-deploy.sh
@@ -16,14 +16,14 @@ docker-compose -f develop-docker-compose.yml up -d --remove-orphans
 
 sleep 5s
 
-docker-compose -f develop-docker-compose.yml exec develop-web rake db:create
+#docker-compose -f develop-docker-compose.yml exec develop-web rake db:create
 
 docker-compose -f develop-docker-compose.yml exec develop-web rake db:migrate
 
-docker-compose -f develop-docker-compose.yml exec develop-web rake db:seed
+#docker-compose -f develop-docker-compose.yml exec develop-web rake db:seed
+
+docker-compose -f develop-docker-compose.yml exec develop-web bundle exec sidekiq -C config/sidekiq.yml -d
 
 docker-compose -f develop-docker-compose.yml exec develop-web whenever --set environment=development --update-crontab
-
-#docker-compose -f develop-docker-compose.yml exec develop-web bundle exec sidekiq -C config/sidekiq.yml -d
 
 exit 0
