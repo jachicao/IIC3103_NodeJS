e49a491c66ae1cf7e4249e6b41881c6c5180f50c
jachicao <jachicao@uc.cl>
2017-05-30 17:33:23 -0400

    migrate

--- master-deploy.sh
+++ master-deploy.sh
@@ -16,7 +16,7 @@ docker-compose -f master-docker-compose.yml up -d --remove-orphans
 
 #docker-compose -f master-docker-compose.yml exec web rake db:create
 
-#docker-compose -f master-docker-compose.yml exec web rake db:migrate
+docker-compose -f master-docker-compose.yml exec web rake db:migrate
 
 #docker-compose -f master-docker-compose.yml exec web rake db:seed
 
