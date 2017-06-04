32b26b4f150abade01d1a87c33f4c7a0d5259261
jachicao <jachicao@uc.cl>
2017-06-04 04:29:07 -0400

    rm log and prune

--- develop-deploy.sh
+++ develop-deploy.sh
@@ -8,6 +8,10 @@ git fetch --all
 
 git reset --hard origin/develop
 
+rm -rf log
+
+docker system prune -f
+
 docker-compose -f develop-docker-compose.yml build
 
 docker-compose -f develop-docker-compose.yml down
@@ -16,11 +20,11 @@ docker-compose -f develop-docker-compose.yml up -d --remove-orphans
 
 sleep 5s
 
-#docker-compose -f develop-docker-compose.yml exec develop-web rake db:create
+#docker-compose -f develop-docker-compose.yml exec develop-web rails db:create
 
-docker-compose -f develop-docker-compose.yml exec develop-web rake db:migrate
+docker-compose -f develop-docker-compose.yml exec develop-web rails db:migrate
 
-#docker-compose -f develop-docker-compose.yml exec develop-web rake db:seed
+#docker-compose -f develop-docker-compose.yml exec develop-web rails db:seed
 
 docker-compose -f develop-docker-compose.yml exec develop-web bundle exec sidekiq -C config/sidekiq.yml -d
 
--- master-deploy.sh
+++ master-deploy.sh
@@ -8,17 +8,21 @@ git fetch --all
 
 git reset --hard origin/master
 
+rm -rf log
+
+docker system prune -f
+
 docker-compose -f master-docker-compose.yml build
 
 docker-compose -f master-docker-compose.yml down
 
 docker-compose -f master-docker-compose.yml up -d --remove-orphans
 
-#docker-compose -f master-docker-compose.yml exec web rake db:create
+#docker-compose -f master-docker-compose.yml exec web rails db:create
 
-docker-compose -f master-docker-compose.yml exec web rake db:migrate
+docker-compose -f master-docker-compose.yml exec web rails db:migrate
 
-#docker-compose -f master-docker-compose.yml exec web rake db:seed
+#docker-compose -f master-docker-compose.yml exec web rails db:seed
 
 docker-compose -f master-docker-compose.yml exec web bundle exec sidekiq -C config/sidekiq.yml -d
 
